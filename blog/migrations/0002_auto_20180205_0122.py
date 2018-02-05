bbcb088baaa130e7c517c3fcd2f6b9b41b942c54
maozi <waixinmaozi@gmail.com>
2018-02-05 01:34:36 +0100

    My Django Girls app, second  commit

---
+++ blog/migrations/0002_auto_20180205_0122.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='author',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to=settings.AUTH_USER_MODEL),
+        ),
+    ]
